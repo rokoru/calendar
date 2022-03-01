ca79bc584b28a8dacc7d22d7ef9ec16c5948459e
rokoru <81513527+rokoru@users.noreply.github.com>
2022-03-01 11:38:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,153 @@
+if __name__ == "__main__":
+
+    animal = 0
+    colour = 0
+    year = 0
+    year = int(input("Введите год\n"))
+    animal = year % 12
+    colour = year % 60
+
+    # 1 цикл-------------------------------------------------
+    if colour == 4 or colour == 5:
+        print("зелен")
+        if animal == 4:
+            print("ая мышь ")
+        elif animal == 5:
+            print("ая корова")
+        elif animal == 6:
+            print("ый тигр")
+        elif animal == 7:
+            print("ый заяц")
+        elif animal == 8:
+            print("ый дракон")
+        elif animal == 9:
+            print("ая змея")
+        elif animal == 10:
+            print("ая лошадь")
+        elif animal == 11:
+            print("ая овца")
+
+    elif animal == 0:
+            print("ая обезьяна")
+    elif animal == 1:
+            print("ая курица")
+    elif animal == 2:
+            print("ая собака")
+    elif animal == 3:
+            print("ая свинья")
+
+    # 2 цикл-------------------------------------------------
+
+    if colour == 6 or colour == 7:
+        print("красн")
+        if animal == 4:
+            print("ая мышь")
+        elif animal == 5:
+            print("ая корова")
+        elif animal == 6:
+            print("ый тигр")
+        elif animal == 7:
+            print("ый заяц")
+        elif animal == 8:
+            print("ый дракон")
+        elif animal == 9:
+            print("ая змея")
+        elif animal == 10:
+            print("ая лошадь")
+        elif animal == 11:
+            print("ая овца")
+        elif animal == 0:
+            print("ая обезьяна")
+        elif animal == 1:
+            print("ая курица")
+        elif animal == 2:
+            print("ая собака")
+        elif animal == 3:
+            print("ая свинья")
+
+    #3 цикл--------------------------------------------------
+
+    if colour == 8 or colour == 9:
+        print("жёлт")
+        if animal == 4:
+            print("ая мышь")
+        elif animal == 5:
+            print("ая корова")
+        elif animal == 6:
+            print("ый тигр")
+        elif animal == 7:
+            print("ый заяц")
+        elif animal == 8:
+            print("ый дракон")
+        elif animal == 9:
+            print("ая змея")
+        elif animal == 10:
+            print("ая лошадь")
+        elif animal == 11:
+            print("ая овца")
+        elif animal == 0:
+            print("ая обезьяна")
+        elif animal == 1:
+            print("ая курица")
+        elif animal == 2:
+            print("ая собака")
+        elif animal == 3:
+            print("ая свинья")
+
+    #4 цикл--------------------------------------------------
+
+    if colour == 0 or colour == 1:
+        print("бел")
+        if animal == 4:
+            print("ая мышь")
+        elif animal == 5:
+            print("ая корова")
+        elif animal == 6:
+            print("ый тигр")
+        elif animal == 7:
+            print("ый заяц")
+        elif animal == 8:
+            print("ый дракон")
+        elif animal == 9:
+            print("ая змея")
+        elif animal == 10:
+            print("ая лошадь")
+        elif animal == 11:
+            print("ая овца")
+        elif animal == 0:
+            print("ая обезьяна")
+        elif animal == 1:
+            print("ая курица")
+        elif animal == 2:
+            print("ая собака")
+        elif animal == 3:
+            print("ая свинья")
+
+    #5 цикл--------------------------------------------------
+
+    if colour == 2 or colour == 3:
+        print("черн")
+        if animal == 4:
+            print("ая мышь")
+        elif animal == 5:
+            print("ая корова")
+        elif animal == 6:
+            print("ый тигр")
+        elif animal == 7:
+            print("ый заяц")
+        elif animal == 8:
+            print("ый дракон")
+        elif animal == 9:
+            print("ая змея")
+        elif animal == 10:
+            print("ая лошадь")
+        elif animal == 11:
+            print("ая овца")
+        elif animal == 0:
+            print("ая обезьяна")
+        elif animal == 1:
+            print("ая курица")
+        elif animal == 2:
+            print("ая собака")
+        elif animal == 3:
+            print("ая свинья")
